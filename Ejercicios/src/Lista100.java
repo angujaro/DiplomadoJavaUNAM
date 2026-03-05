1ea10efa552eeea64d6a7ef0c10b4379c32e3c61
Andrea Jaimes <angujaro@gmail.com>
2026-03-04 22:23:46 -0600

    a

---
+++ Ejercicios/src/Lista100.java
@@ -0,0 +1,12 @@
+public class Lista100 {
+    public static void main(String[] args) {
+        cien();
+    }
+
+    public static void cien(){
+        for(int i=0; i<=100; i++){
+            System.out.println(i);
+        }
+
+    }
+}
